9b6171ec2fa137a44939973a15bcf96e21d2ea42
felixop8 <felixop8@gmail.com>
2017-03-17 11:26:14 -0700

    add feature to clean form after submiting and clean result when resubmiting

--- build/js/app.js
+++ build/js/app.js
@@ -29,7 +29,9 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 $(document).ready(function() {
   $('#doctorForm').submit(function(event) {
     event.preventDefault();
+    $('#result').empty();
     var affection = $('#medicalIssue').val();
+    $('#medicalIssue').val("");
     var doctorFinder = new Doctor();
     doctorFinder.search(affection);
   });
