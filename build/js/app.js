aa39dd6315123ff39a7a517936e40512818c872e
felixop8 <felixop8@gmail.com>
2017-03-17 10:56:16 -0700

    add for loop to append name of the doctors into the html

--- build/js/app.js
+++ build/js/app.js
@@ -9,9 +9,11 @@ function Doctor(){
 
 
 Doctor.prototype.search = function(affection) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ affection + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ affection + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=2&user_key=' + apiKey)
   .then(function(result) {
-     
+    for (var i = 0; i < result.data.length; i++) {
+      console.log(result.data[i].profile.first_name);    
+    }
    })
   .fail(function(error){
      console.log("fail");
