eb12d34721aabb2d1995128c24e2e3a837ff1fa3
felixop8 <felixop8@gmail.com>
2017-03-17 11:15:16 -0700

    query photo url in the api

--- build/js/app.js
+++ build/js/app.js
@@ -12,7 +12,7 @@ Doctor.prototype.search = function(affection) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ affection + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
     for (var i = 0; i < result.data.length; i++) {
-      $('#result').append('<li>' + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + '<img src="' + result.data[i].profile.last_name.image_url + '"></li>');
+      $('#result').append('<li>' + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + '<img src=' + result.data[i].profile.image_url + '></li>');
       console.log(result.data[i].profile.first_name);
     }
    })
